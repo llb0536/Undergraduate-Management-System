30767b63113b85f9a1e8a8f8bc1d9a2864c5ac4d
P.S.V.R <pmq2001@gmail.com>
2011-04-27 15:15:35 +0800

    all

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# -*- encoding : utf-8 -*-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110424130218) do
+ActiveRecord::Schema.define(:version => 20110427051721) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "course_id"
@@ -99,6 +98,16 @@ ActiveRecord::Schema.define(:version => 20110424130218) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "has_read"
+  end
+
+  create_table "programs", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "researches", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "students", :force => true do |t|
